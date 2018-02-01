095d36de0a4cece684f806ede766185114f8d6f5
Tan Theng Lee <thengleetan@gmail.com>
2018-02-01 23:54:04 +0800

    First commit

---
+++ spec/web/features/add_book_spec.rb
@@ -0,0 +1,35 @@
+require 'features_helper'
+
+describe 'Add a book' do
+  after do
+    BookRepository.new.clear
+  end
+
+  it 'can create a new book' do
+    visit '/books/new'
+
+    within 'form#book-form' do
+      fill_in 'Title', with: 'New book'
+      fill_in 'Author', with: 'Some author'
+
+      click_button 'Create'
+    end
+
+    current_path.must_equal('/books')
+    assert page.has_content?('New book')
+  end
+
+  it 'display list of errors when params contains errors' do
+    visit '/books/new'
+
+    within 'form#book-form' do
+      click_button 'Create'
+    end
+
+    current_path.must_equal('/books')
+
+    assert page.has_content?('There was a problem with your submission')
+    assert page.has_content?('Title must be filled')
+    assert page.has_content?('Author must be filled')
+  end
+end
